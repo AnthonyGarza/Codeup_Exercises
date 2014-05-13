f44b032fae3ed117323e3acab0d14c4c86de9df8
Anthony Garza <agarza1972@yahoo.com>
2014-05-13 10:32:45 -0500

    for loop exercise 2

--- for.php
+++ for.php
@@ -8,9 +8,14 @@ fwrite(STDOUT, 'Give me a ending number? ');
 
 $end_num = trim(fgets(STDIN));
 
+// prompt user to choos count increment
+fwrite(STDOUT, 'Give me a number to  increment by? ');
+
+$incrementer = trim(fgets(STDIN));
+
 // for loop
 
-for ($start_num = $start_num; $start_num <= $end_num; $start_num++) {
+for ($start_num = $start_num; $start_num <= $end_num; $start_num += $incrementer) {
 	echo "{$start_num}\n";
 }
 
