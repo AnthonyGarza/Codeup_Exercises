f0df57645dd05f72924265006437fec8353fa317
Anthony Garza <agarza1972@yahoo.com>
2014-05-14 13:32:02 -0500

    switch exercise

---
+++ switch.php
@@ -0,0 +1,39 @@
+<?php
+
+// Set the default timezone
+date_default_timezone_set('America/Chicago');
+
+// Get Day of Week as number
+// 1 (for Monday) through 7 (for Sunday)
+$day_of_week = date('N');
+
+switch($day_of_week) {
+    case 1:
+        // Output Monday
+    	echo "Today is Monday\n";
+    	break;
+    case 2:
+        // Output Tuesday
+    	echo "Today is Tuesday\n";
+    	break;
+    case 3:
+    	// Output Wednesday
+    	echo "Today is Wednesday\n";
+    	break;
+    case 4:
+    	// Output Thursday
+    	echo "Today is Thursday\n";
+    case 5:
+    	// Output Friday
+    	echo "Today is Friday\n";
+    case 6:
+    	// Output Saturday
+    	echo "Today is Saturday\n";
+    case 7:
+    	// Output Sunday
+    	echo "Today is Sunday\n";
+
+    // etc through day 7
+}
+
+?>
