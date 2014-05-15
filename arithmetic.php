bad0e3b0c1a64a84bb133ba7797b6c96fa207eb8
Anthony Garza <agarza1972@yahoo.com>
2014-05-15 12:02:24 -0500

    Validate all the arguments, and display an error if the input is not numeric.

--- arithmetic.php
+++ arithmetic.php
@@ -1,29 +1,61 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+	// validate that values are numeric and display error if not
+    if (is_numeric($a) && is_numeric($b)) {
+    	echo $a + $b . PHP_EOL;
+    } else {
+    	echo 'ERROR: Both $a and $b should be numbers!' . PHP_EOL;
+    }
 }
 
 function subtract($a, $b) {
-    echo $a - $b . PHP_EOL;
+	// validate that values are numeric and display error if not
+    if (is_numeric($a) && is_numeric($b)) {
+        echo $a - $b . PHP_EOL;
+    } else {
+    	echo 'ERROR: Both $a and $b should be numbers!' . PHP_EOL;
+    }
 }
 
 function multiply($a, $b) {
+	// validate that values are numeric and display error if not
+    if (is_numeric($a) && is_numeric($b)) {
     echo $a * $b . PHP_EOL;
+    } else {
+    	echo 'ERROR: Both $a and $b should be numbers!' . PHP_EOL;
+    }
 }
 
 function divide($a, $b) {
+	// validate that values are numeric and display error if not
+    if (is_numeric($a) && is_numeric($b)) {
     echo $a / $b . PHP_EOL;
+    } else {
+    	echo 'ERROR: Both $a and $b should be numbers!' . PHP_EOL;
+    }
 }
 
 function modulus($a, $b) {
+	// validate that values are numeric and display error if not
+    if (is_numeric($a) && is_numeric($b)) {
 	echo $a % $b . PHP_EOL;
+	} else {
+    	echo 'ERROR: Both $a and $b should be numbers!' . PHP_EOL;
+    }
 }
+
 
 add(10, 15);
 subtract(10, 15);
 multiply(6,7);
 divide(42,8);
 modulus(4,3);
+
+add(4,to);
+multiply(2,h);
+subtract(5,4);
+subtract(g,2);
+
 
 ?>
