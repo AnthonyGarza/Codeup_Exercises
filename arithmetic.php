63a80b895adb33c09fa1cf0ea6e2b2184c70535c
Anthony Garza <agarza1972@yahoo.com>
2014-05-16 08:51:55 -0500

    Refactor each function to return the result, removing the echo

--- arithmetic.php
+++ arithmetic.php
@@ -1,58 +1,59 @@
 <?php
 
 function error($a, $b) {
-	echo 'ERROR: Both $a and $b should be numbers!' . PHP_EOL;
+    echo 'ERROR: Both $a and $b should be numbers!' . PHP_EOL;
     echo "Value inputed for $a is \$a and $b is \$b" . PHP_EOL;
 }
 
 function add($a, $b) {
-	// validate that values are numeric and display error if not
+    // validate that values are numeric and display error if not
     if (is_numeric($a) && is_numeric($b)) {
-    	echo $a + $b . PHP_EOL;
+        return $a + $b;
     } else {
-    	error($a, $b);
+        error($a, $b);
     }
 }
 
 function subtract($a, $b) {
-	// validate that values are numeric and display error if not
+    // validate that values are numeric and display error if not
     if (is_numeric($a) && is_numeric($b)) {
-        echo $a - $b . PHP_EOL;
+        return $a - $b;
     } else {
-    	error($a, $b);
+        error($a, $b);
     }
 }
 
 function multiply($a, $b) {
-	// validate that values are numeric and display error if not
+    // validate that values are numeric and display error if not
     if (is_numeric($a) && is_numeric($b)) {
-        echo $a * $b . PHP_EOL;
+        return $a * $b;
     } else {
-    	error($a, $b);
+        error($a, $b);
     }
 }
 
 function divide($a, $b) {
-	// validate that values are numeric and display error if not
+    // validate that values are numeric and display error if not
     if ($b == 0) {
-    	echo 'ERROR: The value of $b can not be 0!' . PHP_EOL;
+        echo 'ERROR: The value of $b can not be 0!' . PHP_EOL;
     } elseif (is_numeric($a) && is_numeric($b)) {
-        echo $a / $b . PHP_EOL;
+        return $a / $b;
     } else {
-    	error($a, $b);
+        error($a, $b);
     }
 }
 
 function modulus($a, $b) {
-	// validate that values are numeric and display error if not
+    // validate that values are numeric and display error if not
     if (is_numeric($a) && is_numeric($b)) {
-	echo $a % $b . PHP_EOL;
-	} else {
-    	error($a, $b);
+    return $a % $b;
+    } else {
+        error($a, $b);
     }
 }
 
-divide(4, 0);
+$sum = add(2, 3);
+echo "The sum of 2 and 3 is {$sum}\n";
 
 
 ?>
