706fe8032cfd43c5fdb01b44a6b679640997c7a3
Anthony Garza <agarza1972@yahoo.com>
2014-05-19 14:57:10 -0500

    created a funtion that returns TRUE/FALSE if array value is found

--- search_arrays.php
+++ search_arrays.php
@@ -1,8 +1,63 @@
 <?php
 
+// Create a file in your exercises directory named search_arrays.php with the following code
+
 // first names
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+// Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names
+
+$query = 'Bob';
+
+function array_has_value($query, $names)
+{
+	if (array_search($query, $names) !== FALSE) {
+		return 'TRUE';
+	} else {
+		return 'FALSE';
+	}
+}
+
+
+
+
+
+//var_dump(array_has_value($query, $names));
+
+// {
+// 	if (array_search($query, $names) !== FALSE) {
+// 		return 'TRUE';
+// 	} else {
+// 		return 'FALSE';
+// 	}
+// }
+
+
+
+
+
+
+
+$result = array_has_value($query, $names);
+
+echo $query . ' is ' . $result . PHP_EOL;
+
+
+
+// Create a function to compare 2 arrays that returns the number of values in common between the arrays.
+// Use the 2 example arrays and make sure your solution uses array_search()
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
