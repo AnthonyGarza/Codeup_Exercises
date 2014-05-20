343a02c192266bbf00619d630ebe9b8f65f1012d
Anthony Garza <agarza1972@yahoo.com>
2014-05-20 14:12:55 -0500

    Turn your solution into a function named humanized_list(). You should be able to pass the $physicists_array as the only argument, and your function will return the result

--- list_with_commas.php
+++ list_with_commas.php
@@ -4,11 +4,24 @@ $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mal
 
 $physicists_array = explode(', ', $physicists_string);
 
-$last = array_pop($physicists_array);
+// $last = array_pop($physicists_array);
 
-$famous_fake_physicists = implode(', ', $physicists_array);
+// $famous_fake_physicists = implode(', ', $physicists_array);
 
-echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists} " ."and {$last}" . PHP_EOL;
+// echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists} " ."and {$last}" . PHP_EOL;
 
-// var_dump($physicists_array);
-// print_r($physicists_array);
+// Converts array into list n1, n2, ..., and n3
+function humanized_list($array) {
+	return implode(', ', $array);
+}
+
+// List of famous peeps
+// $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+// Humanize that list
+$famous_fake_physicists = humanized_list($physicists_array);
+
+// Output sentence
+echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+
+?>
