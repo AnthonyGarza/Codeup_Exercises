fc6af20f9252b5d033bcd91112005d4a4cb586da
Anthony Garza <agarza1972@yahoo.com>
2014-05-20 22:27:14 -0500

    Create a second argument to make alphabetical sorting optional
    Default alphabetical sorting to false. If no args are passed, no sorting takes place

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,8 +1,8 @@
 <?php
 
-$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+// $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
-$physicists_array = explode(', ', $physicists_string);
+// $physicists_array = explode(', ', $physicists_string);
 
 // $last = array_pop($physicists_array);
 
@@ -11,18 +11,28 @@ $physicists_array = explode(', ', $physicists_string);
 // echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists} " ."and {$last}" . PHP_EOL;
 
 // Converts array into list n1, n2, ..., and n3
-function humanized_list($array) {
-	asort($array);
-	return implode(', ', $array);
+function humanized_list($string, $do_sort = FALSE) {
+	$array = explode(', ', $string);
+	if ($do_sort === TRUE) {
+		sort($array);
+	}
+
+	$last_item = array_pop($array);
+
+	array_push($array, "and $last_item");
+
+	$string = implode(', ', $array);
+
+	return $string;
 }
 
 // List of famous peeps
-// $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
 // Humanize that list
-$famous_fake_physicists = humanized_list($physicists_array);
+$famous_fake_physicists = humanized_list($physicists_string, TRUE);
 
 // Output sentence
-echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}." . PHP_EOL;
 
 ?>
