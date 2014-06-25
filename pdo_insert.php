d80a005a451694dcb4c2a8cba76e06dcb1a93ffe
Anthony Garza <agarza1972@yahoo.com>
2014-06-24 23:16:19 -0500

    Corrected location for Insert id#2 and removed qoutes and commas from area_in_acres values

--- pdo_insert.php
+++ pdo_insert.php
@@ -10,16 +10,16 @@ echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
 
 // Create the query and assign to var
 $national_parks = [
-    ['name' => 'Acadia',   'location' => 'Maine', 'date_established' => '1919-02-26', 'area_in_acres' => '47,389.67'],
-    ['name' => 'American Samoa',   'American Samoa' => 'Maine', 'date_established' => '1988-10-31', 'area_in_acres' => '9,000.00'],
-    ['name' => 'Arches',   'location' => 'Utah', 'date_established' => '1971-11-12', 'area_in_acres' => '76,518.98'],
-    ['name' => 'Badlands',   'location' => 'South Dakota', 'date_established' => '1978-11-10', 'area_in_acres' => '242,755.94'],
-    ['name' => 'Big Bend',   'location' => 'Texas', 'date_established' => '1944-06-12', 'area_in_acres' => '801,163.21'],
-    ['name' => 'Biscayne',   'location' => 'Florida', 'date_established' => '1980-06-28', 'area_in_acres' => '172,924.07'],
-    ['name' => 'Black Canyon of the Gunnison',   'location' => 'Colorado', 'date_established' => '1999-10-21', 'area_in_acres' => '32,950.03'],
-    ['name' => 'Bryce Canyon',   'location' => 'Utah', 'date_established' => '1928-02-25', 'area_in_acres' => '35,835.08'],
-    ['name' => 'Canyonlands',   'location' => 'Utah', 'date_established' => '1964-09-12', 'area_in_acres' => '337,597.83'],
-    ['name' => 'Capitol Reef',   'location' => 'Utah', 'date_established' => '1971-12-18', 'area_in_acres' => '241,904.26'],
+    ['name' => 'Acadia',   'location' => 'Maine', 'date_established' => '1919-02-26', 'area_in_acres' => 47389.67],
+    ['name' => 'American Samoa',   'location' => 'American Samoa', 'date_established' => '1988-10-31', 'area_in_acres' => 9000.00],
+    ['name' => 'Arches',   'location' => 'Utah', 'date_established' => '1971-11-12', 'area_in_acres' => 76518.98],
+    ['name' => 'Badlands',   'location' => 'South Dakota', 'date_established' => '1978-11-10', 'area_in_acres' => 242755.94],
+    ['name' => 'Big Bend',   'location' => 'Texas', 'date_established' => '1944-06-12', 'area_in_acres' => 801163.21],
+    ['name' => 'Biscayne',   'location' => 'Florida', 'date_established' => '1980-06-28', 'area_in_acres' => 172924.07],
+    ['name' => 'Black Canyon of the Gunnison',   'location' => 'Colorado', 'date_established' => '1999-10-21', 'area_in_acres' => 32950.03],
+    ['name' => 'Bryce Canyon',   'location' => 'Utah', 'date_established' => '1928-02-25', 'area_in_acres' => 35835.08],
+    ['name' => 'Canyonlands',   'location' => 'Utah', 'date_established' => '1964-09-12', 'area_in_acres' => 337597.83],
+    ['name' => 'Capitol Reef',   'location' => 'Utah', 'date_established' => '1971-12-18', 'area_in_acres' => 241904.26],
 ];
 
 foreach ($national_parks as $national_park) {
